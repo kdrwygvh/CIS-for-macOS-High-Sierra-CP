0d3ba6e69a4ffc67caefba242b96eed54df1cf37
John Hutchison <john@randm.ltd>
2020-03-23 14:39:57 -0400

    Added Jamf ready extension attributes to read Audit Results and Scoring files

--- Extension Attributes/2.5_Audit_List.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Security Reporting - List Risks
-
-auditfile=/Library/Application\ Support/SecurityScoring/org_audit
-echo "<result>$(cat "$auditfile")</result>"
--- Extension Attributes/2.6_Audit_Count.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Security Reporting - Count Risks
-
-auditfile=/Library/Application\ Support/SecurityScoring/org_audit
-echo "<result>$(cat "$auditfile" | grep "*" | wc -l | tr -d '[:space:]')</result>"
